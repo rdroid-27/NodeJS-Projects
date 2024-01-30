733ed405792810dac76128414dfbc4c0ff7cb009
rdroid-27 <gupta27rachit@gmail.com>
2024-01-31 01:41:37 +0530

    Completed the backend of the project and also added a little UI using swagger UI.

--- Job-Portal-App/controllers/jobsController.js
+++ Job-Portal-App/controllers/jobsController.js
@@ -10,11 +10,62 @@ export const createJobController = async (req, res, next) => {
   const job = await jobsModel.create(req.body);
   res.status(201).json({ job });
 };
+
+// GET ALL JOBS
 export const getAllJobsController = async (req, res, next) => {
-  const jobs = await jobsModel.find({ createdBy: req.user.userId });
+  // const jobs = await jobsModel.find({ createdBy: req.user.userId });
+
+  const { status, workType, search, sort } = req.query;
+
+  // conditions for searching filters
+  const queryObject = {
+    createdBy: req.user.userId,
+  };
+
+  // logic filters
+  if (status && status !== "all") {
+    queryObject["status"] = status;
+  }
+  if (workType && workType !== "all") {
+    queryObject["workType"] = workType;
+  }
+  if (search) {
+    queryObject.position = { $regex: search, $options: "i" };
+  }
+
+  let queryResult = jobsModel.find(queryObject);
+
+  // Sorting
+  if (sort === "latest") {
+    queryResult = queryResult.sort("-createdAt");
+  }
+  if (sort === "oldest") {
+    queryResult = queryResult.sort("createdAt");
+  }
+  if (sort === "a-z") {
+    queryResult = queryResult.sort("company");
+  }
+  if (sort === "z-a") {
+    queryResult = queryResult.sort("-company");
+  }
+
+  // pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  queryResult= queryResult.skip(skip).limit(limit);
+  // jobs count
+  const totalJobs= await jobsModel.countDocuments(queryResult);
+  const numOfPages= Math.ceil(totalJobs/limit);
+
+
+
+  const jobs = await queryResult;
+
   res.status(200).json({
-    totalJobs: jobs.length,
+    totalJobs,
     jobs,
+    numOfPages
   });
 };
 
@@ -75,34 +126,43 @@ export const statsJobController = async (req, res, next) => {
   ]);
 
   // default stats
-  const defaultStats={
+  const defaultStats = {
     pending: stats.pending || 0,
     reject: stats.reject || 0,
-    interview: stats.interview ||0
-  }
+    interview: stats.interview || 0,
+  };
   // monthly yearly stats
-  let monthlyApplication= await jobsModel.aggregate([
-{
-  $match:{
-    createdBy: new mongoose.Types.ObjectId(req.user.userId)
-  }
-},
-{
-  $group:{
-    _id: {
-      year:{$year: '$createdAt'},
-      month:{$month: '$createdAt'}
+  let monthlyApplication = await jobsModel.aggregate([
+    {
+      $match: {
+        createdBy: new mongoose.Types.ObjectId(req.user.userId),
+      },
     },
-    count:{
-      $sum:1
-    }
-  }
-}
+    {
+      $group: {
+        _id: {
+          year: { $year: "$createdAt" },
+          month: { $month: "$createdAt" },
+        },
+        count: {
+          $sum: 1,
+        },
+      },
+    },
   ]);
 
-monthlyApplication=monthlyApplication.map((item)=>{
-  const {_id:{year,month},count}=item;
-  const date=moment().month(month-1).year(year).format('MM Y');return {date,count};
-}).reverse();
-  res.status(200).json({ totalJobs: stats.length, stats ,monthlyApplication});
+  monthlyApplication = monthlyApplication
+    .map((item) => {
+      const {
+        _id: { year, month },
+        count,
+      } = item;
+      const date = moment()
+        .month(month - 1)
+        .year(year)
+        .format("MM Y");
+      return { date, count };
+    })
+    .reverse();
+  res.status(200).json({ totalJobs: stats.length, stats, monthlyApplication });
 };
--- Job-Portal-App/server.js
+++ Job-Portal-App/server.js
@@ -1,9 +1,17 @@
+// API Documentation
+import swaggerDoc from "swagger-jsdoc";
+import SwaggerUi from "swagger-ui-express";
+
 // packages Imports
 import express from "express";
-import 'express-async-errors';
+import "express-async-errors";
 import dotenv from "dotenv";
 import cors from "cors";
 import morgan from "morgan";
+
+// security package
+import helmet from "helmet";
+import MongoSanitize from "express-mongo-sanitize";
 
 // files import
 import connectDB from "./config/db.js";
@@ -12,9 +20,28 @@ import errorMiddleware from "./middlewares/errorMiddleware.js";
 // routes imports
 import authRoute from "./routes/authRoute.js";
 import userRoute from "./routes/userRoute.js";
-import jobsRoute from './routes/jobsRoute.js';
+import jobsRoute from "./routes/jobsRoute.js";
+
 // Dotenv config
 dotenv.config();
+
+// swagger api config
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Job Portal Application",
+      description: "NodeJS and ExpressJS based Job Portal",
+    },
+    servers: [
+      {
+        url: `http://localhost:${process.env.PORT}`,
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+const spec = swaggerDoc(options);
 
 //rest object
 const app = express();
@@ -26,6 +53,8 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // middlewares
+app.use(helmet());
+app.use(MongoSanitize());
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
@@ -35,9 +64,11 @@ app.use("/auth", authRoute);
 app.use("/user", userRoute);
 app.use("/job", jobsRoute);
 
+// Home route root
+app.use("/api-doc", SwaggerUi.serve, SwaggerUi.setup(spec));
+
 // validation  middleware
 app.use(errorMiddleware);
-
 
 app.listen(PORT, () => {
   console.log(
